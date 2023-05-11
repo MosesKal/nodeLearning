e488342f46de66e3bf465898980e1ba4c93744ae
MosesK <mosesziongo@gmail.com>
2023-05-11 19:49:09 +0200

    explication des call back

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const path = require("node:path");
+// const path = require("node:path");
 
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-console.log(path.extname(__filename));
-console.log(path.extname(__dirname));
+// console.log(path.extname(__filename));
+// console.log(path.extname(__dirname));
 
-console.log(path.parse(__filename));
-console.log(path.format(path.parse(__filename)));
-console.log(path.isAbsolute(__filename));
-console.log(path.isAbsolute("./data.json"));
-console.log(path.join("folder1", "folder2", "index.js"));
+// console.log(path.parse(__filename));
+// console.log(path.format(path.parse(__filename)));
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute("./data.json"));
+// console.log(path.join("folder1", "folder2", "index.js"));
+
+const greeting = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+const greetingMoses = (greetingFn) => {
+  const name = "MosesDev";
+  greetingFn(name);
+};
+
+greetingMoses(greeting);
