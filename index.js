80c74715c7061e09b914cfc6b22fb3e308074875
MosesK <mosesziongo@gmail.com>
2023-05-11 17:25:12 +0200

    apprentissage du module paht

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ console.log(__dirname);
 
 console.log(path.basename(__filename));
 console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+console.log(path.parse(__filename));
