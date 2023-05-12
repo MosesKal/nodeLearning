e691e778be7c84e00b6a0fe849322b9615a983d1
MosesK <mosesziongo@gmail.com>
2023-05-13 00:51:24 +0200

    mis en place du premier middleware logger

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const { success } = require("./helper");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log(`URL : ${req.url}`);
+  next();
+};
+
+app.use(logger);
+
 app.get("/", (req, res) => res.send("Hello, express!"));
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  res.json(pokemon);
+  const message = "un pokemon a bien ete trouve.";
+  res.json(success(message, pokemon));
 });
 app.get("/api/pokemons", (req, res) => {
-  const pokemonNb = pokemons.length;
-  res.send(`Il ya ${pokemonNb} pokemons dans le pokedex, pour le moment`);
+  const message = "La liste des pokemons a bien ete recuperee";
+  res.json(success(message, pokemons));
 });
 
 app.listen(port, () =>
