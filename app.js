74c8ad2e8bf82e590ff8dda64eb4f2f992d61972
MosesK <mosesziongo@gmail.com>
2023-05-13 10:05:29 +0200

    envoi d'une requette post et reception de donnees sous format JSON

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
-const { success } = require("./helper");
+const bodyParser = require("body-parser");
+const { success, getUniqueId } = require("./helper");
 let pokemons = require("./mock-pokemon");
 
 const app = express();
 const port = 3000;
 
-app.use(favicon(__dirname + "/favicon.ico")).use(morgan("dev"));
+app
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(morgan("dev"))
+  .use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello, express!"));
 app.get("/api/pokemons/:id", (req, res) => {
@@ -19,6 +23,14 @@ app.get("/api/pokemons/:id", (req, res) => {
 app.get("/api/pokemons", (req, res) => {
   const message = "La liste des pokemons a bien ete recuperee";
   res.json(success(message, pokemons));
+});
+
+app.post("/api/pokemons", (req, res) => {
+  const id = getUniqueId(pokemons);
+  const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
+  pokemons.push(pokemonCreated);
+  const message = `Le pokemon ${pokemonCreated}`;
+  res.json(success(message, pokemonCreated));
 });
 
 app.listen(port, () =>
