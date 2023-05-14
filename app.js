395c9b77f8787aef675b498585f49a64328183ec
MosesK <mosesziongo@gmail.com>
2023-05-14 20:25:36 +0200

    end point delete

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/api/pokemons/:id", (req, res) => {
   const message = "un pokemon a bien ete trouve.";
   res.json(success(message, pokemon));
 });
+
 app.get("/api/pokemons", (req, res) => {
   const message = "La liste des pokemons a bien ete recuperee";
   res.json(success(message, pokemons));
@@ -31,6 +32,26 @@ app.post("/api/pokemons", (req, res) => {
   pokemons.push(pokemonCreated);
   const message = `Le pokemon ${pokemonCreated}`;
   res.json(success(message, pokemonCreated));
+});
+
+app.put("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonUpated = { ...req.body, id: id };
+
+  pokemons = pokemons.map((pokemon) => {
+    return pokemon.id === id ? pokemonUpated : pokemon;
+  });
+  const message = `Le pokemon ${pokemonUpated.name} a bien ete modifie`;
+  res.json(success(message, pokemonUpated));
+});
+
+app.delete("/apit/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemonDeleted = pokemons.find((pokemon) => pokemon.id === id);
+
+  pokemons.filter((pokemon) => pokemon.id !== id);
+  const message = `Le pokemon ${pokemonDeleted.name} a bien ete supprime`;
+  res.json(success(message, pokemonDeleted));
 });
 
 app.listen(port, () =>
